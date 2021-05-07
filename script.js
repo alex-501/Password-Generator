e20773fd191df378b3f69f384d47845aee84f1c2
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-07 14:14:37 -0700

    update js

--- script.js
+++ script.js
@@ -1,164 +1,66 @@
 //user input vartiables
 
-var enter;
-var confirmNumber ;
-var confirmCharacter;
-var confirmLowercase;
+var passNum;
+var lowerCase ;
+var upperCase;
+var numbers;
 var confirmUppercase;
 
-// set password variable values:
 
-character = ["!" , "@" , "#", "$", "%" , "^" , "&", "*", "(", ")"]; 
 
-number = [ "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"];
+//generate button, then query selector call by ID
+var generateBtn = document.querySelector("#generate");
 
-alpha = [  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-space = [];
+//generate password function - 
+function generatePassword (){
+  var password =  ""; 
+  var possibleChars = [];
 
-var choices;
+  //prompt variable questions
+  var length =prompt("Choose a length between 8-128 characters")
 
-//convert lowercase to capitol letters
-//remember to study syntax as you go along.
 
-var toUpper = function (x) {
-    return x.toUpperCase();
+  var passNum = parseInt(length); 
+  console.log(passNum)
+  
+  var lowerCase = confirm("LowerCase Letters?")
+  console.log(lowerCase)
 
-};
+  var upperCase = confirm("Capital letters?")
+  console.log(upperCase)
 
-// turn letter to uppercase - variable
+  var numbers = confirm("any Numbers?")
+  console.log(numbers)
+// now connect them
 
-function generatePassword() {
-//ask for user input
-enter = parseInt(prompt("How many Characters? choose between 8 and 28 "));
+
+
+  if(lowerCase)
+  {possibleChars.push("a","b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")}
+
+  if(upperCase)
+  {possibleChars.push("A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")}
+
+  if(numbers) 
+  {possibleChars.push("0" ,"1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" ,  "9" , "10")}
+
+
+    for (var  i=0 ;i< length; i++) {
+      var randomNum = Math.floor(Math.random() * possibleChars.length);
+      var randomChar = possibleChars[randomNum]
+        password = password.concat(randomChar);
+        console.log(password)
+    }
+    
+    //////////////////////////
+      return password
 }
-//if statement
-// remember ! means if its set to false
-if (!enter) {
-    alert("this needs a value");
-}else if (enter < 8 || enter > 128) {
+//write password function 
+function writePassword() 
+{var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+    passwordText.value = password;}
 
-    enter = parseInt(prompt("Seriously? I said between 8 and 128"));
-} else {
-// after once inpiut goes through
-
-confirmNumber = confirm ("Want numbers?");
-confirmCharacter = confirm ("How about special characters?");
-confirmUppercase = confirm ("Aight I see you! how about some uppercase letters?");
-confirmLowercase = confirm ("and  what about Lowercase?");
-
-};
-
-// if no is put for all 4 then:
-
-if (!confirmCharacter && !confirmNumber &&!confirmUppercase && !confirmLowercase)
-{
-
-    choices =alert("come on man. You gotta give me something.");
-}
-
-// apparently I have to set possible outcomes for everything idividually?
-
-// 4 positive responses
-else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) 
-{ choices = character.concat(number, alpha, alpha2);
-
-}
-
-// 3 yes's
-
-else if (confirmCharacter && confirmNumber && confirmUppercase) {
-    choices = character.concat(number, alpha2);
-
-}
-
-else if (confirmCharacter && confirmNumber && confirmLowercase) {
-    choices = character.concat(number, alpha);
-}
-else if (confirmCharacter && confirmLowercase && confirmUppercase) {
-    choices = character.concat(alpha, alpha2);
-}
-else if (confirmNumber && confirmLowercase && confirmUppercase) {
-    choices = number.concat(alpha, alpha2);
-}
-
-// note how we had to do ALL possible outcomes. perhaps I can find a way around this in the future?
-
-// 2 yes's
-
-else if (confirmCharacter && confirmUppercase) {
-    choices = character.concat(number);
-} 
-
-else if(confirmCharacter && confirmLowercase) {
-choices = character.concat(alpha)
-}
-
-else if ( confirmCharacter && confirmUppercase) 
-{
-    choices = character.concat(alpha2)
-
-}
-else if (confirmLowercase && confirmNumber) {
-    choices = alpha.concat(number);
-
-} else if (confirmLowercase && confirmUppercase) {
-    choices = alpha.concat(alpha2);
-
-} else if (confirmNumber && confirmUppercase) {
-    choices = number.concat(alpha2);
-}
-// Else if for 1 positive option
-else if (confirmCharacter) {
-    choices = character;
-}
-else if (confirmNumber) {
-    choices = number;
-}
-else if (confirmLowercase) {
-    choices = alpha;
-}
-
-else if (confirmUppercase) {
-    choices = space.concat(alpha2)
-
-};
-
-var password = [];
-
-// how to random select
-
-for (var i = 0; i < enter; i++) {
-    var pickChoices = choices[Math.floor(math.random() * choices.length)];
-    password.push(pickChoices);
-}
-
-// here you turn password into sentence or a string
-
-var ps = password.join(" ");
-UserInput(ps);
-return ps;
-
-// so I just said ps is set to password.join- which joins them. then tale userinput with password var
-// return password. which is defined above. 
-
-function UserInput(ps) {
-    document.getElementById("password").textContent = ps;
-
-}
-
-var copy = document.querySelector("#copy");
-copy.addEventListener("click" , function (){
-    copyPassword();
-}); 
-
-function copyPassword() {
-    document.getElementById("password").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard!");
-};
-
-
-// Code example demonstrated in a youtube video: 
-// Source: https://youtu.be/9sT03jEwcaw
-// Originally found and studied from https://github.com/jamierachael/Password-Generator/blob/master/script.js}
+//event listener
+generateBtn.addEventListener("click", writePassword);
